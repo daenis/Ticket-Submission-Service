9a9d3bc4c053e8abb008c95acbf5c9054e697310
daenis <dkalaygia@gmail.com>
2022-07-15 09:03:56 -1000

    new endpoint

--- src/main/java/com/api/service/TicketService.java
+++ src/main/java/com/api/service/TicketService.java
@@ -3,6 +3,9 @@ package com.api.service;
 import com.api.dto.Ticket;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.List;
+
 @Service
 public class TicketService {
 
@@ -22,6 +25,13 @@ public class TicketService {
 
     void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public List<Ticket> findTickets() {
+        Ticket ticket = new Ticket();
+        ticket.setDepartment("Software Development");
+        ticket.setIdentifier("ufd8sfsd");
+        return Collections.singletonList(ticket);
     }
 
 }
